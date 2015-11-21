daf4e7546387e3765405d92fb97951c2dea399ce
tammy <943213397@qq.com>
2015-11-21 10:05:41 +0800

    new vector compare

--- two_vector_equal.cpp
+++ two_vector_equal.cpp
@@ -5,23 +5,23 @@ int main()
 {
 	vector<int> ivec1,ivec2;
 	int ival;
- 	cout<<"Enter numbers for vector1(-1 to end):"<<endl;
+	cout<<"Enter numbers for vector1(-1 to end):"<<endl;
 	cin>>ival;
 	while(ival!=-1)
 	{
-	ivec1.push_back(ival);
-	cin>>ival;
+		ivec1.push_back(ival);
+		cin>>ival;
 	}
 	cout<<"Enter numbers for vector2(-1 to end):"<<endl;
 	cin>>ival;
 	while(ival!=-1)
 	{
-	ivec2.push_back(ival);
-	cin>>ival;
+		ivec2.push_back(ival);
+		cin>>ival;
 	}
 	//判断是否相等
 	if(ivec1.size()!=ivec2.size())
-	cout<<"Vector1 is not equal Vector2 !"<<endl;
+		cout<<"Vector1 is not equal Vector2 !"<<endl;
 	else if (ivec1.size() ==0)
 		cout<<"Vector1 is equal Vector2 !"<<endl;
 	else{
@@ -30,19 +30,19 @@ int main()
 		iter2=ivec2.begin();
 		while(iter1!=ivec1.end()&&iter2!=ivec2.end()&&*iter1==*iter2)
 		{
-		   ++iter1;
-		   ++iter2;
+			++iter1;
+			++iter2;
 		}
 		if (iter1==ivec1.end())
-		cout<<"Vector1 is equal Vector2 !"<<endl;
+			cout<<"Vector1 is equal Vector2 !"<<endl;
 		else
-		cout<<"Vector1 is not equal Vector2 !"<<endl;
-	    }
-  	return 0;
+			cout<<"Vector1 is not equal Vector2 !"<<endl;
+	}
+	return 0;
 
-		
-		
-	
-	
+
+
+
+
 }
 
