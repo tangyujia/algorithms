248a04a0539018d36064306c5df4d9e968521030
tammy <943213397@qq.com>
2015-11-28 09:05:33 +0800

    string vector copy into charcter arrays

---
+++ character_arrays_vector.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<string.h>
+using namespace std;
+int main()
+{
+	vector<string> svec;
+	string str;
+	cout<<"Enter strings:(ctrl +z to end)"<<endl;
+	while(cin>>str)
+		svec.push_back(str);
+	char **parr=new char *[svec.size()];
+	size_t ix=0;
+	for(vector<string>::iterator iter=svec.begin();iter!=svec.end();++iter,++ix)
+	{
+		char *p=new char[(*iter).size()+1];
+		strcpy(p,(*iter).c_str());
+		parr[ix]=p;
+	}
+	cout<<"Content of vector:"<<endl;
+	for(vector<string>::iterator iter2=svec.begin();iter2!=svec.end();++iter2)
+		cout<<*iter2<<endl;
+	cout<<"Content of character arrays:"<<endl;
+	for(ix=0;ix!=svec.size();++ix)
+		cout<<parr[ix]<<endl;
+
+	for(ix=0;ix!=svec.size();++ix)
+		delete[] parr[ix];
+	delete [] parr;
+	return 0;
+}
